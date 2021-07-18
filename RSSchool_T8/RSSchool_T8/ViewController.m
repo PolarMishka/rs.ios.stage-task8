18d03202d1de54bbf34ca1f94b5705dc16325b5d
Mikhail Kuzin <kuzin-ms@mail.ru>
2021-07-19 00:44:52 +0300

    feat: add DrawwingsViewController and some func

--- RSSchool_T8/RSSchool_T8/ViewController.m
+++ RSSchool_T8/RSSchool_T8/ViewController.m
@@ -6,8 +6,15 @@
 //
 
 #import "ViewController.h"
+#import "DrawingsViewController.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *loginField;
+@property (weak, nonatomic) IBOutlet UIButton *openPaletteButton;
+@property (weak, nonatomic) IBOutlet UIButton *openTimerButton;
+@property (weak, nonatomic) IBOutlet UIButton *drawButton;
+@property (weak, nonatomic) IBOutlet UIButton *shareButton;
 
 @end
 
@@ -16,6 +23,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = UIColor.grayColor;
+    
+    UIButton *openPaletteButton = [[UIButton alloc] initWithFrame:CGRectMake(100.0, 200.0, 100.0, 100.0)];
+    openPaletteButton.backgroundColor = [UIColor redColor];
+    [self.view addSubview:openPaletteButton];
+    
+    UIButton *openTimerButton = [[UIButton alloc] initWithFrame:CGRectMake(100.0, 200.0, 100.0, 100.0)];
+    
+    UIButton *drawButton = [[UIButton alloc] initWithFrame:CGRectMake(100.0, 200.0, 100.0, 100.0)];
+    
+    UIButton *shareButton = [[UIButton alloc] initWithFrame:CGRectMake(100.0, 200.0, 100.0, 100.0)];
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self setupNavigationItems];
+}
+
+- (void)setupNavigationItems {
+
+    self.navigationItem.title = @"Artist";
+    
+    UIBarButtonItem *next = [[UIBarButtonItem alloc] initWithTitle:@"Drawings"
+                                                             style:UIBarButtonItemStylePlain
+                                                            target:self
+                                                            action:@selector(nextTapped:)];
+    self.navigationItem.rightBarButtonItem = next;
+}
+
+- (void)nextTapped:(id)sender {
+    DrawingsViewController *viewController = [DrawingsViewController new];
+    [self.navigationController pushViewController:viewController animated:YES];
 }
 
 
